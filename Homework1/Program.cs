4c742f076f8b232658cd7a0baa507c9730b0bde1
amporah <107694746+amporah@users.noreply.github.com>
2022-06-17 19:12:56 +0400

    Add files via upload

---
+++ Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}! Сегодня {DateTime.Now}."); ;
+        }
+    }
+}
